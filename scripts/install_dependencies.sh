a9746044e111ceb8310c28689e6c4effe0a49b29
kirihar2 <kirihara.juran@gmail.com>
2019-05-27 14:36:57 -0700

    commit

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #sudo yum install -y python
-pip install -U -r ../requirements_pip.txt
+
+pip install -U -r ${PWD}/requirements_pip.txt
 mkdir weights
 mkdir data
-pip install ../twitters_gen_package
 
-python web/backend/manage.py makemigrations
-python web/backend/manage.py migrate
+pip install -U ${PWD}/twitters_gen_package
+
+python ${PWD}/web/backend/manage.py makemigrations
+python ${PWD}/web/backend/manage.py migrate
