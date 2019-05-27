381692953163a6165b9bc69040b2f32569f00dc2
kirihar2 <kirihara.juran@gmail.com>
2019-05-27 14:25:26 -0700

    commit

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-sudo yum install -y python
+#sudo yum install -y python
 pip install -y -r ../requirements_pip.txt
+mkdir weights
+mkdir data
+pip install ../twitters_gen_package
+
+python web/backend/manage.py makemigrations
+python web/backend/manage.py migrate
---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo Starting backend django server 
+python web/backend/manage.py runserver
